a827084a09cd4ff1a9f4d7c54299fdb2e0590721
Malena Guallar <malena.guallar@gmail.com>
2024-05-29 16:50:52 +0200

    feat: structure of page

--- src/app/[locale]/about/about.tsx
+++ src/app/[locale]/about/about.tsx
@@ -5,74 +5,82 @@ import "@/lib/env";
 
 import IntroBlock from "@/components/IntroBlock";
 import TitleBlock from "@/components/TitleBlock";
+import AboutSection from "@/components/AboutSection";
 import { useTranslations } from "next-intl";
 
-import "@/styles/globals.css"
+import "@/styles/globals.css";
 
 const About = () => {
-
-  const t = useTranslations("about")
+  const t = useTranslations("about");
 
   return (
     <>
-      <IntroBlock title={t("title")}/>
-      <WhySection />
-      <WhoSection />
-      <MethodsSection />
+      <IntroBlock title={t("title")} />
+
+      <TitleBlock title={t("why.title")} id="whySectionTitle" />
+      <AboutSection content={t("why.content")}/>
+
+      <TitleBlock title={t("who.title")} id="whoSectionTitle" />
+      <AboutSection content={t("who.content")}/>
+
+      <TitleBlock title={t("methods.title")} id="methodsSectionTitle" />
+      <AboutSection content={t("methods.content")} />
+
+      <IntroBlock title={t("sources")} />
+
+      <TitleBlock title={t("macro.title")} id="macroSectionTitle" />
+      <AboutSection content={t("macro.content00")}/>
+      <AboutSection content={t("macro.table")}/>
+      <AboutSection content={t("macro.content01")}/>
+
+      <TitleBlock title={t("alternatives.title")} id="alternativesSectionTitle" />
+      <AboutSection content={t("alternatives.content")}/>
+
+      <TitleBlock title={t("companies.title")} id="companiessSectionTitle" />
+      <AboutSection content={t("companies.content")}/>
+      <AboutSection content={t("companies.farms_map.title")} classname="uppercase font-bold"/>
+      <AboutSection content={t("companies.farms_map.data.title")} classname="p-leavy"/>
+      <AboutSection content={t("companies.farms_map.data.content")}/>
+      <AboutSection content={t("companies.farms_map.carbon.title")} classname="p-leavy"/>
+      <AboutSection content={t("companies.farms_map.carbon.content")} />
+      <AboutSection content={t("companies.farms_map.limits.title")} classname="p-leavy"/>
+      <AboutSection content={t("companies.farms_map.limits.content")} />
+      <AboutSection content={t("companies.farms_companies.title")} classname="uppercase font-bold"/>
+      <AboutSection content={t("companies.farms_companies.content")}/>
+      <AboutSection content={t("companies.farms_figures.title")} classname="uppercase font-bold"/>
+      <AboutSection content={t("companies.farms_figures.content")}/>
+
+
+      <TitleBlock title={t("biodiversity.title")} id="biodiversitySectionTitle"/>
+      <AboutSection content={t("biodiversity.deforestation.title")} classname="p-leavy"/>
+      <AboutSection content={t("biodiversity.deforestation.content")}/>
+      <AboutSection content={t("biodiversity.evasion.title")} classname="p-leavy"/>
+      <AboutSection content={t("biodiversity.evasion.content")}/>
+
+
+      <TitleBlock title={t("human_health.title")} id="humanHealthSectionTitle"/>
+      <AboutSection content={t("human_health.antibiotics.title")} classname="p-leavy"/>
+      <AboutSection content={t("human_health.antibiotics.content")}/>
+      <AboutSection content={t("human_health.microplastics.title")} classname="p-leavy"/>
+      <AboutSection content={t("human_health.microplastics.content")}/>
+
+
+      <TitleBlock title={t("animal_welfare.title")} id="animalWelfareSectionTitle"/>
+      <AboutSection content={t("animal_welfare.density.title")} classname="p-leavy"/>
+      <AboutSection content={t("animal_welfare.density.content")}/>
+      <AboutSection content={t("animal_welfare.mortality.title")} classname="p-leavy"/>
+      <AboutSection content={t("animal_welfare.mortality.content")}/>
+
+
+      <TitleBlock title={t("climate.title")} id="climateSectionTitle"/>
+      <AboutSection content={t("climate.content")}/>
+
+      
+      <TitleBlock title={t("social.title")} id="socialWelfareSectionTitle"/>
+      <AboutSection content={t("social.content")}/>
+
     </>
   );
 };
 
 export default About;
-
-
-const WhySection = () => {
-  const t = useTranslations("about")
-  return (
-    <>
-      <TitleBlock title={t("why.title")} id="whySectionTitle"/>
-      <p className="p-leavy">{t("why.content")}</p>
-      <ul className="list-disc">
-        <li>{t("why.list.01")}</li>
-        <li>{t("why.list.02")}</li>
-        <li>{t("why.list.03")}</li>
-      </ul>
-    </>
-  )
-}
-
-const WhoSection = () => {
-  const t = useTranslations("about")
-  return (
-    <>
-      <TitleBlock title={t("who.title")} id="whoSectionTitle"/>
-      <p className="p-leavy">{t("who.content")}</p>
-      <ul className="list-disc">
-        <li>{t("who.list.01")}</li>
-        <li>{t("who.list.02")}</li>
-      </ul>
-    </>
-  )
-}
-
-const MethodsSection = () => {
-  const t = useTranslations("about")
-  return (
-    <>
-      <TitleBlock title={t("methods.title")} id="methodsSectionTitle"/>
-      <p className="p-leavy">{t("methods.content.01")}</p>
-      <p className="p-leavy">{t("methods.content.02")}</p>
-      <ul className="list-decimal">
-        <li>{t("methods.list01.01")}</li>
-        <li>{t("methods.list01.02")}</li>
-        <li>{t("methods.list01.03")}</li>
-      </ul>
-      <p className="p-leavy">{t("methods.content.03")}</p>
-      <ul className="list-decimal">
-        <li>{t("methods.list02.01")}</li>
-        <li>{t("methods.list02.02")}</li>
-        <li>{t("methods.list02.03")}</li>
-      </ul>
-    </>
-  )
-}
