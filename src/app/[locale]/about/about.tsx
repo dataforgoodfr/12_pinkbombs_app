ab058e5f66556f240e127629f418ec537a461ecd
Malena Guallar <malena.guallar@gmail.com>
2024-05-31 17:57:13 +0200

    fix: about page updates

--- src/app/[locale]/about/about.tsx
+++ src/app/[locale]/about/about.tsx
@@ -6,7 +6,6 @@ import * as React from "react";
 import "@/lib/env";
 
 import AboutSection from "@/components/AboutSection";
-import IntroBlock from "@/components/IntroBlock";
 import TitleBlock from "@/components/TitleBlock";
 
 const About = () => {
@@ -14,19 +13,13 @@ const About = () => {
 
   return (
     <>
-      <IntroBlock
-        title={t("title")}
-        className="flex p-6 lg:p-12 text-red1 bg-pink1 min-h-[40px] lg:min-h-[300px]"
-      />
+      <TitleBlock title={t("title")} />
 
       <WhySection />
       <WhoSection />
       <MethodsSection />
 
-      <IntroBlock
-        title={t("sources")}
-        className="flex p-6 lg:p-12 text-red1 bg-pink1 min-h-[40px] lg:min-h-[300px]"
-      />
+      <TitleBlock title={t("sources")} />
 
       <MacroSection />
       <AlternativesSection />
@@ -47,8 +40,11 @@ const WhySection = () => {
 
   return (
     <>
-      <TitleBlock title={t("why.title")} id="whySectionTitle" />
-      <AboutSection content={t("why.content")} />
+      <AboutSection
+        content={t("why.content")}
+        title={t("why.title")}
+        id="why-section-title"
+      />
     </>
   );
 };
@@ -58,8 +54,11 @@ const WhoSection = () => {
 
   return (
     <>
-      <TitleBlock title={t("who.title")} id="whoSectionTitle" />
-      <AboutSection content={t("who.content")} />
+      <AboutSection
+        content={t("who.content")}
+        title={t("who.title")}
+        id="who-section-title"
+      />
     </>
   );
 };
@@ -69,8 +68,11 @@ const MethodsSection = () => {
 
   return (
     <>
-      <TitleBlock title={t("methods.title")} id="methodsSectionTitle" />
-      <AboutSection content={t("methods.content")} />
+      <AboutSection
+        content={t("methods.content")}
+        title={t("methods.title")}
+        id="methods-section-title"
+      />
     </>
   );
 };
@@ -80,8 +82,11 @@ const MacroSection = () => {
 
   return (
     <>
-      <TitleBlock title={t("macro.title")} id="macroSectionTitle" />
-      <AboutSection content={t("macro.content")} />
+      <AboutSection
+        content={t("macro.content")}
+        title={t("macro.title")}
+        id="macro-section-title"
+      />
     </>
   );
 };
@@ -91,11 +96,11 @@ const AlternativesSection = () => {
 
   return (
     <>
-      <TitleBlock
+      <AboutSection
+        content={t("alternatives.content")}
         title={t("alternatives.title")}
-        id="alternativesSectionTitle"
+        id="alternatives-section-title"
       />
-      <AboutSection content={t("alternatives.content")} />
       <Link
         className="inline-flex justify-center mx-6 md:mx-12 xl:mx-[60px] mb-10 p-4 min-w-40 lg:text-2xl text-red1 bg-tranparent font-secondary uppercase rounded-xl tracking-widest border-2 font-bold border-red1 hover:bg-red1 hover:text-white transition-all ease-in-out duration-100"
         href={t("alternatives.link")}
@@ -111,48 +116,11 @@ const CompaniesSection = () => {
 
   return (
     <>
-      <TitleBlock title={t("companies.title")} id="companiessSectionTitle" />
-      <AboutSection content={t("companies.content")} />
       <AboutSection
-        content={t("companies.mowi.title")}
-        classname="uppercase font-bold"
+        content={t("companies.content")}
+        title={t("companies.title")}
+        id="companies-section-title"
       />
-      <AboutSection content={t("companies.mowi.content")} />
-      <AboutSection
-        content={t("companies.reports.title")}
-        classname="uppercase font-bold"
-      />
-      <AboutSection content={t("companies.reports.content")} />
-
-      <AboutSection
-        content={t("companies.farms_map.title")}
-        classname="uppercase font-bold"
-      />
-      <AboutSection
-        content={t("companies.farms_map.data.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("companies.farms_map.data.content")} />
-      <AboutSection
-        content={t("companies.farms_map.carbon.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("companies.farms_map.carbon.content")} />
-      <AboutSection
-        content={t("companies.farms_map.limits.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("companies.farms_map.limits.content")} />
-      <AboutSection
-        content={t("companies.farms_companies.title")}
-        classname="uppercase font-bold"
-      />
-      <AboutSection content={t("companies.farms_companies.content")} />
-      <AboutSection
-        content={t("companies.farms_figures.title")}
-        classname="uppercase font-bold"
-      />
-      <AboutSection content={t("companies.farms_figures.content")} />
     </>
   );
 };
@@ -162,20 +130,11 @@ const BiodiversitySection = () => {
 
   return (
     <>
-      <TitleBlock
+      <AboutSection
+        content={t("biodiversity.content")}
         title={t("biodiversity.title")}
-        id="biodiversitySectionTitle"
+        id="biodiversity-section-title"
       />
-      <AboutSection
-        content={t("biodiversity.deforestation.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("biodiversity.deforestation.content")} />
-      <AboutSection
-        content={t("biodiversity.evasion.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("biodiversity.evasion.content")} />
     </>
   );
 };
@@ -185,20 +144,11 @@ const HumanSection = () => {
 
   return (
     <>
-      <TitleBlock
+      <AboutSection
+        content={t("human_health.content")}
         title={t("human_health.title")}
-        id="humanHealthSectionTitle"
+        id="human-health-section-title"
       />
-      <AboutSection
-        content={t("human_health.antibiotics.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("human_health.antibiotics.content")} />
-      <AboutSection
-        content={t("human_health.microplastics.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("human_health.microplastics.content")} />
     </>
   );
 };
@@ -208,20 +158,11 @@ const AnimalSection = () => {
 
   return (
     <>
-      <TitleBlock
+      <AboutSection
+        content={t("animal_welfare.content")}
         title={t("animal_welfare.title")}
-        id="animalWelfareSectionTitle"
+        id="animal-welfare-section-title"
       />
-      <AboutSection
-        content={t("animal_welfare.density.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("animal_welfare.density.content")} />
-      <AboutSection
-        content={t("animal_welfare.mortality.title")}
-        classname="p-leavy"
-      />
-      <AboutSection content={t("animal_welfare.mortality.content")} />
     </>
   );
 };
@@ -231,8 +172,11 @@ const ClimateSection = () => {
 
   return (
     <>
-      <TitleBlock title={t("climate.title")} id="climateSectionTitle" />
-      <AboutSection content={t("climate.content")} />
+      <AboutSection
+        content={t("climate.content")}
+        title={t("climate.title")}
+        id="climate-section-title"
+      />
     </>
   );
 };
@@ -241,8 +185,11 @@ const SocialSection = () => {
   const t = useTranslations("about");
   return (
     <>
-      <TitleBlock title={t("social.title")} id="socialWelfareSectionTitle" />
-      <AboutSection content={t("social.content")} />
+      <AboutSection
+        content={t("social.content")}
+        title={t("social.title")}
+        id="social-section-title"
+      />
     </>
   );
 };
--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -3,18 +3,22 @@ import React from "react";
 
 const AboutSection = ({
   content,
-  classname = "p",
+  className = "",
+  title,
+  id = ""
 }: {
   content: string;
-  classname?: string;
+  className?: string;
+  title: string;
+  id?: string;
 }) => {
   const t = useTranslations();
+  if (!content) return <></>;
+
   return (
-    <div className="flex justify-center flex-col mx-6 md:mx-12 xl:mx-[60px]">
-      <p
-        dangerouslySetInnerHTML={{ __html: t.raw(content) }}
-        className={classname}
-      ></p>
+    <div className={className}>
+      <div id={id} dangerouslySetInnerHTML={{ __html: t.raw(title) }} className="h3 p-6 md:p-12 max-w-[1596px] mx-auto prose"></div>
+      <div dangerouslySetInnerHTML={{ __html: t.raw(content) }} className="p-6 md:p-12 max-w-[1596px] mx-auto prose"></div>
     </div>
   );
 };
