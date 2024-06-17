26cd001675b162acccc3c58fc61acc2ea063eae2
Thomas Catinaud Taris <tcatinaud-taris@greenspector.com>
2024-06-17 18:37:42 +0200

    Fix lint

--- src/components/Edito.tsx
+++ src/components/Edito.tsx
@@ -1,8 +1,8 @@
 import clsx from "clsx";
 import Image from "next/image";
-import Link from "next/link";
 import { useTranslations } from "next-intl";
 import React from "react";
+
 import MainLink from "@/components/links/MainLink";
 
 type EditoProps = {
