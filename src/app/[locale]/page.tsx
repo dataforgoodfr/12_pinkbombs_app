916e204811eabaa05654ed87b92bf876e6067bfb
gmguarino <gmguarino1@gmail.com>
2024-10-12 16:44:01 +0200

    fix: ran format

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -117,12 +117,12 @@ const VideoSection = () => {
     <div className="p-6 lg:px-12 lg:pt-36 lg:pb-64 text-red1 bg-pink1">
       <div className="max-w-[1500px] mx-auto">
         <div className="pt-[56.25%] relative">
-          <iframe 
+          <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/YPVHTczRyuU?si=BZA4TaWW86aayGRp&rel=0" 
-            title="Pinkbombs" 
-            allow="autoplay; fullscreen; picture-in-picture; clipboard-write" 
-            referrerPolicy="strict-origin-when-cross-origin" 
+            src="https://www.youtube.com/embed/YPVHTczRyuU?si=BZA4TaWW86aayGRp&rel=0"
+            title="Pinkbombs"
+            allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+            referrerPolicy="strict-origin-when-cross-origin"
             allowFullScreen
           />
         </div>
