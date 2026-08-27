625d145ff2a832ca0c7670eb2ab8743e6806c553
Aurélie A. <anglio.aurelie@gmail.com>
2026-08-27 19:54:56 -0400

    (fix): Improve intro layout and ul tag fix

--- src/app/[locale]/(site)/about/about.tsx
+++ src/app/[locale]/(site)/about/about.tsx
@@ -40,12 +40,12 @@ const IntroSection = () => {
 
   return (
     <section className="bg-v2-pink text-v2-blue">
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 py-12 lg:py-0 px-10 md:px-12">
-        <div className="col-start-2 xl:col-start-3 flex flex-col gap-4 lg:pt-52 xl:pt-152 xl:-ml-[250px]">
-          <h1 className="h1 text-pretty lg:max-w-[60%] xl:max-w-[80%]">
+      <div className="relative grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 py-12 lg:py-0 px-10 md:px-12">
+        <div className="relative lg:top-20 col-start-2 xl:col-start-3 flex flex-col gap-4 lg:pt-32 lg:pl-12 xl:pt-152 xl:-ml-[250px]">
+          <h1 className="h1 text-pretty lg:max-w-[60%]">
             {t("intro.title")}
           </h1>
-          <p className="p-lead text-pretty xl:max-w-[80%]">
+          <p className="p-lead text-pretty lg:max-w-[80%] xl:max-w-[100%]">
             {t("intro.caption")}
           </p>
         </div>
@@ -120,13 +120,13 @@ const ExplanationSection = () => {
               {t("why.reaction.title")}
             </h2>
           </div>
-          <p className="text-pretty preserve-lines">
+          <div className="text-pretty preserve-lines">
             {t.rich("why.reaction.paragraph", {
               strong: (chunks) => <strong>{chunks}</strong>,
               ul: (chunks) => <ul>{chunks}</ul>,
               li: (chunks) => <li className="list-disc ml-5">{chunks}</li>,
             })}
-          </p>
+          </div>
         </div>
       </div>
     </section>
